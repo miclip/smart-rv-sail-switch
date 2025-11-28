924cfd169dcfeee98e30f1881e34d505e4c87b75
miclip <mlipscombe@outlook.com>
2025-11-28 12:38:40 -0700

    Remove confirmation

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -5,13 +5,6 @@ set -e
 cd "$(dirname "$0")/.."
 
 echo "Uploading to ATtiny85 via USBtinyISP..."
-echo "Make sure:"
-echo "  - USBtinyISP is connected to USB"
-echo "  - ATtiny85 is in the programmer socket"
-echo "  - Power is connected (if using external power)"
-echo ""
-read -p "Press Enter to continue or Ctrl+C to cancel..."
-
 ~/.platformio/penv/bin/pio run -e attiny85 -t upload
 
 echo ""
